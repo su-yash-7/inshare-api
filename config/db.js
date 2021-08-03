fadc555ae2e0a5482c34d9fc1ff07dbd65c77505
su-yash-7 <suyashpradhan7@gmail.com>
2021-08-03 15:55:48 +0530

    Mongo db connection

---
+++ config/db.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+function connectDB() {
+  mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  });
+  const connection = mongoose.connection;
+  connection
+    .once('open', () => {
+      console.log('Database connected 🥳🥳🥳🥳');
+    })
+    .catch((err) => {
+      console.log('Connection failed 🙁🙁🙁🙁');
+    });
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const connectDB = require('./config/db');
+connectDB();
+
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
