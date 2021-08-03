28d018830ee9322fed35540901fb6a30c8830531
su-yash-7 <suyashpradhan7@gmail.com>
2021-08-03 22:48:22 +0530

    Upload file endpoint

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/db');
 connectDB();
 
+app.use('/api/files', require('./routes/files'));
+
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
