52d186c96241cd3731ad7e979f912c4d57dd0575
su-yash-7 <suyashpradhan7@gmail.com>
2021-08-04 15:11:30 +0530

    Download page

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+app.use(express.static('public'));
 
 const connectDB = require('./config/db');
 connectDB();
 
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
 app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
