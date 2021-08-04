05ac63aa85d3208b282150e1e5cc04c226d6c454
su-yash-7 <suyashpradhan7@gmail.com>
2021-08-04 23:17:34 +0530

    added cors setup

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const cors = require('cors');
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(','),
+};
+
+app.use(cors(corsOptions));
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
 connectDB();
+
 app.use(express.json());
 
 app.set('views', path.join(__dirname, '/views'));
